37e77c34a92ca215907e3686b001a5f03321846a
Abbas <amirabbas.samadiiiii@gmail.com>
2023-09-06 15:35:19 +0330

    'Add__button_1__in_calculator'

--- Calculator.py
+++ Calculator.py
@@ -3,9 +3,22 @@ from tkinter import *
 root = Tk()
 root.title('Calculator')
 root.geometry('500x600')
+
+def structure():
+    input_1 = Entry_1.get()
+    print(input_1)
+    # print(int(input_1))
+    # print(type(3*60))
+    
+
+
 #Tag section
-lble = Entry(root,width=14,font=("Comic Sans MS",40,"bold"))
-lble.place(x=24,y=10)
+Entry_1 = Entry(root,width=14,font=("Comic Sans MS",40,"bold"))
+Entry_1.place(x=24,y=10)
+
+#button section
+button_1 = Button(root,text="=",font=("Comic Sans MS",30,"bold"),command=structure)
+button_1.place(x=430,y=500)
 
 
 
