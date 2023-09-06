1e7cb1f0c223a599981bbbd6d8d312b9cbaadd94
AmirAbbasSM <amirabbas.samadiiiii@gmail.com>
2023-09-06 10:44:09 +0330

    'Add_Entry_in_calculator'

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+root.title('Calculator')
+root.geometry('500x600')
+#Tag section
+lble = Entry(root,width=14,font=("Comic Sans MS",40,"bold"))
+lble.place(x=24,y=10)
+
+
+
+root.mainloop()
